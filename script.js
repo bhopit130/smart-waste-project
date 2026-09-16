8c4bed66d18f6ca2bc478273c9eaa9a832584de0
bhopit130 <87403573+bhopit130@users.noreply.github.com>
2026-09-16 16:06:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -610,7 +610,7 @@ async function captureAndAnalyzeWithGroq() {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "meta-llama/llama-4-scout-17b-16e-instruct",
+                model: "llama-3.2-11b-vision-preview",
                 messages: [
                     {
                         role: "user",
